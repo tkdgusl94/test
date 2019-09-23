525e16702636667dadd2c2c07c88e7aa1ed72b97
sanghyun <tkdgusl94@naver.com>
2019-09-23 19:36:04 +0900

    routing 모듈화

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express()
 var bodyParser = require('body-parser') // npm install body-parser --save 바디파서는 post할때 받기 위해서 입력한 값을 받기 위해 사용
 var cors = require('cors') // npm install cors --save, localhost랑 127.0.0.1를 같게 해주는 모듈
 var mysql = require('mysql')
+var main = require('./router/main')
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -15,6 +16,8 @@ var connection = mysql.createConnection({
 connection.connect() //<ALTER USER ‘root’@’localhost’ IDENTIFIED WITH mysql_native_password BY ‘사용할패스워드’> mysql에 들어가서 입력하기.
 
 app.use(cors())
+app.use('/main', main)
+
 app.listen(3000, function() {
     console.log("start, express server on port 3000");
 });
@@ -26,10 +29,6 @@ app.set('view engine', 'ejs') // npm install ejs --save
 
 app.get('/', function(req, res) {
     console.log('test');
-    res.sendFile(__dirname + '/public/main.html')
-});
-
-app.get('/main', function(req, res) {
     res.sendFile(__dirname + '/public/main.html')
 });
 
