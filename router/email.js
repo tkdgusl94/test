bd16014ec294272fe49c6e3548e3e4cd29f37818
sanghyun <tkdgusl94@naver.com>
2019-09-24 12:40:08 +0900

    routing 모듈화2(DB연결부분)

--- router/email.js
+++ router/email.js
@@ -1,11 +1,6 @@
 var express = require('express')
-var app = express()
 var router = express.Router();
-var bodyParser = require('body-parser') // npm install body-parser --save 바디파서는 post할때 받기 위해서 입력한 값을 받기 위해 사용
 var mysql = require('mysql')
-
-router.use(bodyParser.json()) // body-parser를 json 형태로 받기 위함.
-router.use(bodyParser.urlencoded({ extended: true }))
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -14,7 +9,6 @@ var connection = mysql.createConnection({
     password: 'qlrvka94!',
     database: 'test'
 })
-
 connection.connect() //<ALTER USER ‘root’@’localhost’ IDENTIFIED WITH mysql_native_password BY ‘사용할패스워드’> mysql에 들어가서 입력하기.
 
 router.post('/post', function(req, res) {
