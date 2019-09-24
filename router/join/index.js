0fee008cd698148e53f20b813c47b7ac356b5783
sanghyun <tkdgusl94@naver.com>
2019-09-24 15:57:03 +0900

    passport기반 세션처리

--- router/join/index.js
+++ router/join/index.js
@@ -21,17 +21,26 @@ router.get('/', function(req, res) {
     res.render('join.ejs', { 'message': msg });
 });
 
+passport.serializeUser(function(user, done) {
+    console.log('passport session save : ', user.id)
+    done(null, user.id);
+})
+
+passport.deserializeUser(function(id, done) {
+    console.log('passport session get_id : ', id)
+    done(null, id);
+})
 passport.use('local-join', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true // 인증을 수행하는 인증 함수로 HTTP request를 그대로 전달할지 여부를 결정한다.
 }, function(req, email, password, done) {
-    var query = connection.query('select * from user where email = ?', [email], function(err, rows) {
+    var query = connection.query('select * from user where email = ?', [email], function(err, rows) { // email이 DB에 저장되어 있는지 체크
         if (err) return done(err);
-        if (rows.length) {
+        if (rows.length) { // DB에 이미 있으면 에러처리
             console.log('existed user')
             return done(null, false, { message: 'your email is already used' })
-        } else {
+        } else { // DB에 없는 경우 DB에 넣어줌
             var sql = { email: email, pw: password };
             var query = connection.query('insert into user set ?', sql, function(err, rows) {
                 if (err) throw err
--- router/main/main.js
+++ router/main/main.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var path = require('path')
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/main.html'))
+    console.log('main js loaded', req.user)
+    var id = req.user;
+    //res.sendFile(path.join(__dirname, '../../public/main.html'))
+    res.render('main.ejs', { 'id': id })
 });
 
 module.exports = router;
