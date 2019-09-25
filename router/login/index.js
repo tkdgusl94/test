09b3a4dd06250ff4ed1b465487e6d8df3a00b9e6
sanghyun <tkdgusl94@naver.com>
2019-09-25 21:07:38 +0900

    Ajax 기반의 passport 인증처리

--- router/login/index.js
+++ router/login/index.js
@@ -37,23 +37,24 @@ passport.use('local-login', new LocalStrategy({
 }, function(req, email, password, done) {
     var query = connection.query('select * from user where email = ?', [email], function(err, rows) { // email이 DB에 저장되어 있는지 체크
         if (err) return done(err);
-        if (rows.length) { // DB에 이미 있으면 에러처리
-            console.log('existed user')
-            return done(null, false, { message: 'your email is already used' })
-        } else { // DB에 없는 경우 DB에 넣어줌
-            var sql = { email: email, pw: password };
-            var query = connection.query('insert into user set ?', sql, function(err, rows) {
-                if (err) throw err
-                return done(null, { 'email': email, 'id': rows.insertId })
-            })
+        if (rows.length) { // login 정보가 DB에 있는 경우
+            return done(null, { 'email': email, 'id': rows[0].UID })
+        } else { // DB에 정보가 없는 경우 에러처리. 여기서 반환된 값이 밑의 ***** 쪽으로 넘어감
+            return done(null, false, { 'message': 'your login info is not found >.<' });
         }
     })
 }));
 
-router.post('/', passport.authenticate('local-join', {
-    successRedirect: '/main',
-    failureRedirect: '/join',
-    failureFlash: true
-}))
+router.post('/', function(req, res, next) {
+    passport.authenticate('local-login', function(err, user, info) {
+        if (err) res.status(500).json(err);
+        if (!user) return res.status(401).json(info.message);
+
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.json(user); // *****
+        });
+    })(req, res, next);
+})
 
 module.exports = router;
